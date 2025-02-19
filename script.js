f9e767fe0a053e8438230600001d8d41d60338a0
EmeraldVoid <69017131+EmeraldVoid@users.noreply.github.com>
2025-02-19 03:45:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const homeButton = document.getElementById('homeButton');
+    const twitchButton = document.getElementById('twitchButton');
+    const githubButton = document.getElementById('githubButton');
+  
+    homeButton.addEventListener('click', function() {
+      window.location.href = 'https://emeraldvoid.github.io/pokemon-challenges/';
+    });
+  
+    twitchButton.addEventListener('click', function() {
+      window.open('https://www.twitch.tv/emerald_void', '_blank');
+    });
+  
+    githubButton.addEventListener('click', function() {
+      window.open('https://www.github.com/emeraldvoid', '_blank');
+    });
+  });
+  
